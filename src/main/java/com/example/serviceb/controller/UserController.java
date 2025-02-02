ca648921a66772a0526fa8a64588c1439efea183
Pratul <pratul@cdot.in>
2025-02-02 16:05:20 +0530

    modified controller

--- src/main/java/com/example/serviceb/controller/UserController.java
+++ src/main/java/com/example/serviceb/controller/UserController.java
@@ -1,25 +1,38 @@
 package com.example.serviceb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/serviceB")
 public class UserController {
 
     @Autowired
     RestTemplate restTemplate;
+
+    @Value("${PORT_VAL}")
+    private String port_val;
 
     @GetMapping("/callA")
     public String callA(){
 
         System.out.println("Inside call A controller at service B");
 
-        String response=restTemplate.getForObject("http://SERVICEA/helloA",String.class);
+        String response=restTemplate.getForObject("http://SERVICEA/serviceA/hello",String.class);
         System.out.println("response:"+response);
         return response;
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("inside hello B controller of service B");
+        System.out.println("hello B.....");
+        return "hello B from port:"+port_val;
     }
 }
